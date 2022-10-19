d731a4ce9d67087dd889c48ee7a7e08ea306cc16
kennedyyakubu <kennedyyakubu56@gmail.com>
2022-10-18 17:36:47 -0700

    islower, updated

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -10,13 +10,26 @@
  *    
  * Return: 1 if character is lowercase, 0 otherwise.
  *
- **/
-int _islower(int c)
+**/
+int r;
 
-{
-		if (c >= 'a' && c <= 'z')
 
-			return (1);
-		else
-			return (0);
-}
+
+    r = _islower('H');
+
+        _putchar(r + '0');
+
+	    r = _islower('o');
+
+	        _putchar(r + '0');
+
+		    r = _islower(108);
+
+		        _putchar(r + '0');
+
+			    _putchar('\n');
+
+			        return (0);
+
+				}
+
